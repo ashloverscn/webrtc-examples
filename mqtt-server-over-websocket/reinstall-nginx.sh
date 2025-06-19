508036f5ec96f02cef2d40f944da30bb287b4be7
ashloverscn <ashloverscn@gmail.com>
2025-06-20 00:59:49 +0530

    Update reinstall-nginx.sh

--- mqtt-server-over-websocket/reinstall-nginx.sh
+++ mqtt-server-over-websocket/reinstall-nginx.sh
@@ -97,10 +97,10 @@ server {
     location /socket.io/ {
         proxy_pass http://localhost:3000/socket.io/;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
     }
 
     location /health {
